67ea1b31228c0c5c1a05f7e2582391818720b21b
khaledhusain <khaledhusain21@gmail.com>
2026-02-24 15:44:52 +0000

    feat: add time range util function

--- backend/app/utils/time.js
+++ backend/app/utils/time.js
@@ -1,1 +1,9 @@
-// Date and math functions 
+function getDaysForRange(range) {
+    if (range === "last-night") return 1;
+    if (range === "last-week") return 7;
+    if (range === "last-month") return 30;
+}
+
+module.exports = {
+    getDaysForRange
+};
