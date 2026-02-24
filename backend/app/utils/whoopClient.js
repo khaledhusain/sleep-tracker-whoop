318efa14e4e455c52663b1e4e42e6abce5146197
khaledhusain <khaledhusain21@gmail.com>
2026-02-24 15:43:47 +0000

    feat: add initial whoopClient file structure

--- backend/app/utils/whoopClient.js
+++ backend/app/utils/whoopClient.js
@@ -1,1 +1,19 @@
-// This file will contain the code to interact with the Whoop API
+const axios = require('axios');
+
+function exchangeCodeForToken(code) {
+    return axios.post();
+}
+
+function refreshAccessToken(refreshToken) {
+    return axios.post();
+}
+
+function getSleep(accessToken) {
+    return axios.get();
+}
+
+module.exports = {
+    exchangeCodeForToken,
+    refreshAccessToken,
+    getSleep
+};
