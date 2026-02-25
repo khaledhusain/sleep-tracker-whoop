85a3aa6f2a727da4148a018ee7fed283c7969ce6
khaledhusain <khaledhusain21@gmail.com>
2026-02-25 01:18:23 +0000

    feat(whoop-auth): implement starter token validationmiddleware (not final)

--- backend/app/middleware/whoopAuthMiddleware.js
+++ backend/app/middleware/whoopAuthMiddleware.js
@@ -1,1 +1,19 @@
-//test branch
+const whoopClient = require('../utils/whoopClient');
+const whoopModels = require('../models/whoop.server.models');
+
+function whoopAuthMiddleware(req, res, next) {
+    whoopModels.getTokens((err, tokens) => {
+      if (err) {
+        return res.status(500).json({ error: "Failed to get tokens" });
+      }
+  
+      if (!tokens) {
+        return res.status(401).json({ error: "Not authenticated" });
+      }
+  
+      req.accessToken = tokens.access_token;
+      next();
+    });
+  }
+
+module.exports = whoopAuthMiddleware;
