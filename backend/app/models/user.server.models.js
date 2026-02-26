8bb3c2d83b324b68d05ad892b30d52f70021163b
EllisOllier <ellisollier1@gmail.com>
2026-02-26 15:44:02 +0000

    Add authenticateUser, getToken, setToken, removeToken, getIdFromToken and getUser to user model

--- backend/app/models/user.server.models.js
+++ backend/app/models/user.server.models.js
@@ -13,14 +13,81 @@ const addNewUser = (user, done) => {
     let values = [user.first_name, user.last_name, user.email, hash, salt.toString('hex')];
 
     db.run(query, values, function (err) {
-        if (err) {
-            console.log(err);
-            return done(err);
-        }
+        if (err) return done(err);
         return done(null, this.lastID);
     });
 };
 
+const authenticateUser = (email, password, done) => {
+    const query = "SELECT user_id, password, salt FROM users where email=?"
+
+    db.get(query, [email], (err, row) => {
+        if (err) return done(err);
+        if (!row) return done(404); // Incorrect email
+
+        if (row.salt === null) row.salt = '';
+
+        let salt = Buffer.from(row.salt, 'hex');
+
+        if (row.password === getHash(password, salt)) {
+            return done(false, row.user_id)
+        } else {
+            return done(404) // Incorrect password
+        }
+    });
+};
+
+const getToken = (id, done) => {
+    const query = "SELECT session_token FROM users WHERE user_id=?";
+    db.get(query, id, (err, row) => {
+        if (err) return done(err);
+        if (!row) return done(404); // Incorrect user_id
+        return done(false, row.session_token)
+    })
+};
+
+const setToken = (id, done) => {
+    let token = crypto.randomBytes(16).toString('hex');
+
+    const query = "UPDATE users SET session_token=? WHERE user_id=?";
+
+    db.run(query, [token, id], (err) => {
+        return done(err, token);
+    });
+};
+
+const removeToken = (token, done) => {
+    const query = "UPDATE users SET session_token=null WHERE session_token=?";
+
+    db.run(query, [token], (err) => {
+        return done(err);
+    })
+};
+
+const getIdFromToken = (token, done) => {
+    const query = "SELECT user_id FROM users WHERE session_token = ?";
+
+    db.get(query, [token], (err, row) => {
+        if (err) return done(err);
+        if (!row || !token) return done(err);
+
+        return done(null, row.user_id);
+    })
+};
+
+const getUser = (user_id, done) => {
+    const query = "SELECT user_id, first_name, last_name FROM users WHERE user_id=?";
+    db.get(query, user_id, (err, row) => {
+        if (err) return done(err);
+        if (!row) return done(404);
+
+        return done(false, { "user_id": row.user_id, "first_name": row.first_name, "last_name": row.last_name });
+    })
+}
+
 module.exports = {
     addNewUser: addNewUser,
+    authenticateUser: authenticateUser,
+    getToken: getToken,
+    setToken: setToken,
 }
