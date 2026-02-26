e0ef862385623c85ff88a4c91ad2433b080edcda
EllisOllier <ellisollier1@gmail.com>
2026-02-26 15:44:19 +0000

    Add login route

--- backend/app/routes/user.server.routes.js
+++ backend/app/routes/user.server.routes.js
@@ -1,10 +1,10 @@
 const users = require("../controllers/user.server.controllers");
 
 module.exports = function(app){
-    app.route("/user")
+    app.route("/user/create_account")
         .post(users.create_account)// users.create_account
-    // app.route("/users/login")
-    //     .post() // users.login
+    app.route("/user/login")
+        .post(users.login) // users.login
     // app.route("logout")
     //     .post() // middleware.authenticate, users.logout
 }
