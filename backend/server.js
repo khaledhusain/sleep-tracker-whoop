50066fb1cd9fcf0fa37e32247f33217e0d9ef6a0
EllisOllier <ellisollier1@gmail.com>
2026-02-26 15:56:11 +0000

    Add user routes import

--- backend/server.js
+++ backend/server.js
@@ -40,6 +40,7 @@ app.get('/dbhealth', (req, res) => {
 
 // Call routes here
 require('./app/routes/test.server.routes')(app);
+require('./app/routes/user.server.routes.js')(app);
 
 // Default response for any other request
 app.use((req, res) => {
