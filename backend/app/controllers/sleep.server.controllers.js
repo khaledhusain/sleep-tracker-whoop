545569124a1b31a3a855e28a1272cac840d6327a
IsaacRid <isaac.rideal@gmail.com>
2026-02-27 19:39:54 +0000

    feat: Added validation for get_sleep controller

--- backend/app/controllers/sleep.server.controllers.js
+++ backend/app/controllers/sleep.server.controllers.js
@@ -3,9 +3,20 @@ const joi = require(`joi`);
 const get_all_sleeps = (req, res) => {
     
 }
+
 const get_sleep = (req, res) => {
-    
+    const getSleepSchema = joi.object({
+        id: joi.number().integer().positive().required()
+    });
+
+    const { error } = getSleepSchema.validate(req.body)
+    if (error) {
+        return res.status(400).send({
+            "error_message": error,
+        })
+    }
 }
+
 const delete_sleep = (req, res) => {
     
 }
