b49a565ac747c98bf1a81f105cc0bdc352844b69
IsaacRid <isaac.rideal@gmail.com>
2026-02-27 16:05:05 +0000

    feat: Added all /sleep routes

--- backend/app/routes/sleep.server.routes.js
+++ backend/app/routes/sleep.server.routes.js
@@ -2,5 +2,7 @@ const sleep = require("../controllers/sleep.server.controllers");
 const authMiddleware = require("../middleware/authMiddleware");
 
 module.exports = function(app){
-    
+    app.route("/sleep")
+        .get(authMiddleware.authenticate, sleep.get_all_sleeps)
+
 }
