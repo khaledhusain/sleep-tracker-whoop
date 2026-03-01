521bfee36403efd0b2c8dc968eacd6313b832694
IsaacRid <isaac.rideal@gmail.com>
2026-03-01 20:42:25 +0000

    feat: added basic validation for create_sleep

--- backend/app/controllers/sleep.server.controllers.js
+++ backend/app/controllers/sleep.server.controllers.js
@@ -1,7 +1,30 @@
-const joi = require(`joi`);
+const Joi = require(`joi`);
 
 const create_sleep = (req, res) => {
-    
+    const createSleepSchema = Joi.object({
+        whoop_record_id: Joi.string().optional(),
+        date: Joi.date().iso().required(),
+        nap: Joi.boolean().optional(),
+        bedtime: Joi.string().isoDate().required(),
+        wake_time: Joi.string().isoDate().required(),
+        total_in_bed_minutes: Joi.number().integer().min(0).optional(),
+        total_sleep_duration_minutes: Joi.number().integer().min(0).optional(),
+        light_sleep_minutes: Joi.number().integer().min(0).optional(),
+        deep_sleep_minutes: Joi.number().integer().min(0).optional(),
+        rem_sleep_minutes: Joi.number().integer().min(0).optional(),
+        awake_minutes: Joi.number().integer().min(0).optional(),
+        sleep_performance_score: Joi.number().integer().min(0).optional(),
+        sleep_efficiency: Joi.number().integer().min(0).optional(),
+        sleep_consistency: Joi.number().integer().min(0).optional(),
+        respiratory_rate: Joi.number().optional(),
+    });
+
+    const { error } = getAllSleepsSchema.validate(req.body);
+    if (error) {
+        return res.status(400).send({
+            "error_message": error,
+        })
+    }
 }
 
 const get_all_sleeps = (req, res) => {
@@ -19,8 +42,8 @@ const get_all_sleeps = (req, res) => {
 }
 
 const get_sleep = (req, res) => {
-    const getSleepSchema = joi.object({
-        id: joi.number().integer().positive().required()
+    const getSleepSchema = Joi.object({
+        id: Joi.number().integer().positive().required()
     });
 
     const { error } = getSleepSchema.validate(req.body)
@@ -32,8 +55,8 @@ const get_sleep = (req, res) => {
 }
 
 const delete_sleep = (req, res) => {
-    const deleteSleepSchema = joi.object({
-        id: joi.number().integer().positive().required()
+    const deleteSleepSchema = Joi.object({
+        id: Joi.number().integer().positive().required()
     });
 
     const { error } = deleteSleepSchema.validate(req.body)
@@ -48,4 +71,5 @@ module.exports = {
     get_all_sleeps,
     get_sleep,
     delete_sleep,
+    create_sleep
 }
