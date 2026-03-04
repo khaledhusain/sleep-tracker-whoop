b8f113f3505f70bcd892e5cf0d372e3f100e3c30
EllisOllier <ellisollier1@gmail.com>
2026-03-04 12:48:49 +0000

    Add invalid user logout unit tests

---
+++ backend/test/invalid.user.logout.test.js
@@ -0,0 +1,23 @@
+const request = require("supertest");
+const app = require("../server");
+
+describe('POST /user/logout', () => {
+    it("should return 401 no user with the token is found", async () => {
+        const response = await request(app)
+            .post('/user/logout')
+            .set('X-Authorization', 'zzzf91fe0523bda3e1b326d31fea0zzz');
+        
+        expect(response.statusCode).toBe(401);
+        expect(response.body).toHaveProperty('message');
+    })
+})
+
+describe('POST /user/logout', () => {
+    it("should return 401 no user with the token is found", async () => {
+        const response = await request(app)
+            .post('/user/logout')
+        
+        expect(response.statusCode).toBe(401);
+        expect(response.body).toHaveProperty('message');
+    })
+})
