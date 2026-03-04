090184a3671429da6689973f7eb64778ed8b3b40
EllisOllier <ellisollier1@gmail.com>
2026-03-04 16:06:03 +0000

    Update code arrangement on server.js

--- backend/server.js
+++ backend/server.js
@@ -3,34 +3,24 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
-const whoopRoutes = require("./app/routes/whoop.server.routes");
 
 const db = require(`./app/utils/database.js`);
-
 const app = express();
+
 app.use(cors());
-
-// Server port
-const HTTP_PORT = 3333;
-
-// Start server
-app.listen(HTTP_PORT, () => {
-    console.log('Server running on port: ' + HTTP_PORT);
-});
-
-// Logging
 app.use(morgan('tiny'));
-
-// Body parser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// Root endpoint
+require('./app/routes/test.server.routes')(app);
+require('./app/routes/whoop.server.routes')(app);
+require('./app/routes/user.server.routes.js')(app);
+require(`./app/routes/sleep.server.routes.js`)(app);
+
 app.get('/', (req, res, next) => {
     res.json({ 'status': 'Alive' });
 });
 
-// DB health endpoint
 app.get('/dbhealth', (req, res) => {
     try {
         db.prepare('SELECT 1').get();
@@ -40,15 +30,13 @@ app.get('/dbhealth', (req, res) => {
     }
 });
 
-// Call routes here
-require('./app/routes/test.server.routes')(app);
-require('./app/routes/whoop.server.routes')(app);
-require('./app/routes/user.server.routes.js')(app);
-require(`./app/routes/sleep.server.routes.js`)(app);
-
-// Default response for any other request
 app.use((req, res) => {
     res.sendStatus(404);
 });
 
-module.exports = app;
+const HTTP_PORT = 3333;
+app.listen(HTTP_PORT, () => {
+    console.log('Server running on port: ' + HTTP_PORT);
+});
+
+module.exports = { app, db };
