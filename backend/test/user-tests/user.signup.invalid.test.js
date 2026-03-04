bbf2b53821950216f966805495e3cff86deb8c69
EllisOllier <ellisollier1@gmail.com>
2026-03-04 15:58:14 +0000

    Move user tests to user-tests directory

---
+++ backend/test/user-tests/user.signup.invalid.test.js
@@ -0,0 +1,197 @@
+const request = require("supertest");
+const app = require("../../server");
+
+describe('POST /user/create_account - missing first_name field', () => {
+    it("should return 400 with error_message and validation details", async () => {
+        const user = {
+            last_name: "Doe",
+            email: "janedoe@doemail.com",
+            password: "Password123!"
+        };
+
+        const response = await request(app)
+            .post('/user/create_account')
+            .send(user)
+
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('error_message');
+    });
+})
+
+describe('POST /user/create_account - missing last_name field', () => {
+    it("should return 400 with error_message and validation details", async () => {
+        const user = {
+            first_name: "Jane",
+            email: "janedoe@doemail.com",
+            password: "Password123!"
+        };
+
+        const response = await request(app)
+            .post('/user/create_account')
+            .send(user)
+
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('error_message');
+    });
+})
+
+
+describe('POST /user/create_account - missing email field', () => {
+    it("should return 400 with error_message and validation details", async () => {
+        const user = {
+            first_name: "Jane",
+            last_name: "Doe",
+            password: "Password123!"
+        };
+
+        const response = await request(app)
+            .post('/user/create_account')
+            .send(user)
+
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('error_message');
+    });
+})
+
+describe('POST /user/create_account - missing password field', () => {
+    it("should return 400 with error_message and validation details", async () => {
+        const user = {
+            first_name: "Jane",
+            last_name: "Doe",
+            email: "janedoe@doemail.com"
+        };
+
+        const response = await request(app)
+            .post('/user/create_account')
+            .send(user)
+
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('error_message');
+    });
+})
+
+describe('POST /user/create_account - invalid first_name field', () => {
+    it("should return 400 with error_message and validation details", async () => {
+        const user = {
+            first_name: "Jan3",
+            last_name: "Doe",
+            email: "janedoe@doemail.com",
+            password: "Password123!"
+        };
+
+        const response = await request(app)
+            .post('/user/create_account')
+            .send(user)
+
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('error_message');
+    });
+})
+
+describe('POST /user/create_account - invalid last_name field', () => {
+    it("should return 400 with error_message and validation details", async () => {
+        const user = {
+            first_name: "Jane",
+            last_name: "D0e",
+            email: "janedoe@doemail.com",
+            password: "Password123!"
+        };
+
+        const response = await request(app)
+            .post('/user/create_account')
+            .send(user)
+
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('error_message');
+    });
+})
+
+describe('POST /user/create_account - invalid email field, missing @ symbol', () => {
+    it("should return 400 with error_message and validation details", async () => {
+        const user = {
+            first_name: "Jane",
+            last_name: "Doe",
+            email: "janedoedoemail.com",
+            password: "Password123!"
+        };
+
+        const response = await request(app)
+            .post('/user/create_account')
+            .send(user)
+
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('error_message');
+    });
+})
+
+describe('POST /user/create_account - invalid email field, missing email top level domain', () => {
+    it("should return 400 with error_message and validation details", async () => {
+        const user = {
+            first_name: "Jane",
+            last_name: "Doe",
+            email: "janedoe@doemail",
+            password: "Password123!"
+        };
+
+        const response = await request(app)
+            .post('/user/create_account')
+            .send(user)
+
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('error_message');
+    });
+})
+
+describe('POST /user/create_account - invalid password, missing upper case character', () => {
+    it("should return 400 with error_message and validation details", async () => {
+        const user = {
+            first_name: "Jane",
+            last_name: "Doe",
+            email: "janedoe@doemail",
+            password: "password123!"
+        };
+
+        const response = await request(app)
+            .post('/user/create_account')
+            .send(user)
+
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('error_message');
+    });
+})
+
+describe('POST /user/create_account - invalid password, missing special character', () => {
+    it("should return 400 with error_message and validation details", async () => {
+        const user = {
+            first_name: "Jane",
+            last_name: "Doe",
+            email: "janedoe@doemail.com",
+            password: "Password123"
+        };
+
+        const response = await request(app)
+            .post('/user/create_account')
+            .send(user)
+
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('error_message');
+    });
+})
+
+describe('POST /user/create_account - invalid password, missing numbers', () => {
+    it("should return 400 with error_message and validation details", async () => {
+        const user = {
+            first_name: "Jane",
+            last_name: "Doe",
+            email: "janedoe@doemail.com",
+            password: "PasswordIsMine!"
+        };
+
+        const response = await request(app)
+            .post('/user/create_account')
+            .send(user)
+
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('error_message');
+    });
+})
