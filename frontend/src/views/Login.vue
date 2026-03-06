cdf14a2abf3ff51833931a6da24d7a4cbb7c7d77
ace <grace@graceb.net>
2026-03-06 11:55:58 +0000

    6.3

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,12 +1,21 @@
 <template>
     <div>
+        <!-- title and icon -->
+        <div class="w-full flex flex-col items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-8">
+                <path fill-rule="evenodd"
+                    d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z"
+                    clip-rule="evenodd" />
+            </svg>
+            <h1 class="text-2xl font-bold">Welcome back</h1>
+            <h2 class="mb-5">Sign in to continue tracking your sleep</h2>
+        </div>
+
         <!-- form -->
         <form @submit.prevent="handleSubmit" class="flex flex-col items-center">
-            <!-- title -->
-            <h2 class="text-2xl font-bold mb-5">Sign in </h2>
-
             <div>
-                <input class="block bg-grey-3 rounded w-70 p-1.5 placeholder-blue-4 focus:ring-2 focus:ring-blue-4 focus:outline-none text-blue-3"
+                <input
+                    class="block bg-grey-3 rounded w-70 p-1.5 placeholder-blue-4 focus:ring-2 focus:ring-blue-4 focus:outline-none text-blue-3"
                     v-model="email" placeholder="Email">
                 <div v-show="submitted && !email">Email is required</div>
             </div>
@@ -14,7 +23,8 @@
             <br />
 
             <div>
-                <input class="block bg-grey-3 rounded w-70 p-1.5 placeholder-blue-4 focus:ring-2 focus:ring-blue-4 focus:outline-none text-blue-3"
+                <input
+                    class="block bg-grey-3 rounded w-70 p-1.5 placeholder-blue-4 focus:ring-2 focus:ring-blue-4 focus:outline-none text-blue-3"
                     v-model="password" placeholder="Password">
                 <div v-show="submitted && !password">Password is required</div>
             </div>
--- frontend/src/views/SignUp.vue
+++ frontend/src/views/SignUp.vue
@@ -1,9 +1,17 @@
 <template>
     <div>
+        <!-- title and icon -->
+        <div class="w-full flex flex-col items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-8">
+                <path fill-rule="evenodd"
+                    d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z"
+                    clip-rule="evenodd" />
+            </svg>
+            <h1 class="text-2xl font-bold mb-5">Sign up</h1>
+        </div>
+
         <!-- form -->
         <form @submit.prevent="handleSubmit" class="flex flex-col items-center">
-            <!-- title -->
-            <h2 class="text-2xl font-bold mb-5">Sign up</h2>
 
             <div class="flex flex-row gap-6">
                 <div>
@@ -83,6 +91,12 @@ export default {
 
             if (!(emailValidator.validate(email))) {
                 this.error = "Email must be valid";
+                return;
+            }
+
+            const passPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^A-Za-z0-9]).+$/
+            if (!(passPattern.test(password))) {
+                this.error = "Password is not strong enough";
                 return;
             }
 
