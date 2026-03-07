833c997ca8d238926d0751b101a39eaa4cbbed24
JoshuaR132 <jrroberts141@gmail.com>
2026-03-07 11:25:07 +0000

    Added more to the start page so that it looks less empty

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -8,7 +8,7 @@
       
       <div class="flex flex-col items-center mt-12">
         <div class="relative w-24 h-24 mb-6 flex items-center justify-center rounded-full shadow-[0_0_40px_rgba(153,163,251,0.2)] border border-blue-4/30">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-12 h-12 text-white">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-12 h-12 text-white" aria-hidden="true">
            <path fill-rule="evenodd"
            d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z"
            clip-rule="evenodd" />
@@ -21,6 +21,21 @@
         <p class="text-grey-1 mt-3 text-center text-sm font-medium">
           Understand your rest.
         </p>
+      </div>
+
+      <div class="flex flex-col gap-4 my-8 text-grey-2 text-sm font-medium w-full px-6">
+        <div class="flex items-center gap-4 bg-blue-2/30 p-3 rounded-lg border border-blue-4/20">
+          <svg class="w-6 h-6 text-purple" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path></svg>
+          <span>Track sleep cycles</span>
+        </div>
+        <div class="flex items-center gap-4 bg-blue-2/30 p-3 rounded-lg border border-blue-4/20">
+          <svg class="w-6 h-6 text-purple" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"></path></svg>
+          <span>Analyse sleep trends</span>
+        </div>
+        <div class="flex items-center gap-4 bg-blue-2/30 p-3 rounded-lg border border-blue-4/20">
+          <svg class="w-6 h-6 text-purple" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>
+          <span>Wake up refreshed</span>
+        </div>
       </div>
 
       <div class="w-full flex flex-col gap-4 mb-6">
